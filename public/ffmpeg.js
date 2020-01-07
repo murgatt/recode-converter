3b4cfb50dcd9bff59e7d4544afd198553281c972
murgatt <theomurgat@hotmail.fr>
2020-01-07 14:07:25 +0100

    feat: main converter with simple UI

--- public/ffmpeg.js
+++ public/ffmpeg.js
@@ -9,7 +9,7 @@ const convert = ({ input }) => {
     console.log('RUN CMD', input);
 
     return new Promise((resolve, reject) => {
-        ffmpeg(SAMPLE)
+        ffmpeg(input)
             .on('start', commandLine => {
                 console.log(`Spawned Ffmpeg with command: ${commandLine}`);
             })
@@ -22,24 +22,8 @@ const convert = ({ input }) => {
                 resolve({ convertedFilePath: OUTPUT });
             })
             .on('progress', progress => console.log(`PROGRESS: ${progress.percent}`))
-            .outputOptions('-map', '0', '-c', 'copy', '-c:a', 'ac3')
+            // .outputOptions('-map', '0', '-c', 'copy', '-c:a', 'ac3')
             .saveToFile(OUTPUT);
-    });
-};
-
-const listFiles = () => {
-    const directoryPath = path.resolve(SAMPLE);
-    //passsing directoryPath and callback function
-    fs.readdir(directoryPath, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-        //listing all files using forEach
-        files.forEach(function (file) {
-            // Do whatever you want to do with the file
-            console.log(file);
-        });
     });
 };
 
