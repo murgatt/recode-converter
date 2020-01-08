221debb55d7f75a8ab810b2c80b1d4d63548af63
Théo Murgat <theomurgat@hotmail.fr>
2020-01-08 23:16:52 +0100

    feat: add i18next & react-i18next (#3)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'typeface-roboto';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './i18n';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
