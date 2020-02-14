1cc821f1dffa19068c768e7b8655f93a36a7b917
murgatt <theomurgat@hotmail.fr>
2020-02-14 02:04:45 +0100

    add bitrate options for other codecs

--- src/config/conversion/audioSettings.js
+++ src/config/conversion/audioSettings.js
@@ -109,6 +109,70 @@ export const bitrate = {
             },
         ],
     },
+    dts: {
+        options: [
+            {
+                label: i18n.t('default'),
+                value: 'default',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 320 }),
+                value: '320k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 448 }),
+                value: '448k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 640 }),
+                value: '640k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 768 }),
+                value: '768k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 1536 }),
+                value: '1536k',
+            },
+        ],
+    },
+    eac3: {
+        options: [
+            {
+                label: i18n.t('default'),
+                value: 'default',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 192 }),
+                value: '192k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 320 }),
+                value: '320k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 448 }),
+                value: '448k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 640 }),
+                value: '640k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 1024 }),
+                value: '1024k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 2048 }),
+                value: '2048k',
+            },
+            {
+                label: i18n.t('conversionSettings.bitrateValue', { value: 4096 }),
+                value: '4096k',
+            },
+        ],
+    },
 };
 
 export default {
