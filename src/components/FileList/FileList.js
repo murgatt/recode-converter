67bbe88808aa9183d95f689e2b361c491d6cce9c
murgatt <theomurgat@hotmail.fr>
2020-02-18 13:57:00 +0100

    add clear file list button

--- src/components/FileList/FileList.js
+++ src/components/FileList/FileList.js
@@ -1,11 +1,21 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core';
+import ClearIcon from '@material-ui/icons/ClearAll';
 import File from '../File';
 import { getFileIds, getFilesById } from '../../store/file/file.selectors';
-import { deleteFiles } from '../../store/file/file.actions';
+import { clearFiles, deleteFiles } from '../../store/file/file.actions';
+import IconButton from '../IconButton';
 
 const useStyles = makeStyles(theme => ({
+    clearButton: {
+        bottom: theme.spacing(),
+        left: 0,
+        margin: '0 auto',
+        position: 'absolute',
+        right: 0,
+    },
     fab: {
         bottom: theme.spacing(2),
         position: 'absolute',
@@ -24,20 +34,47 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const FileList = () => {
+    const { t } = useTranslation();
     const dispatch = useDispatch();
     const classes = useStyles();
     const fileIds = useSelector(getFileIds);
     const filesById = useSelector(getFilesById);
+    const [isHover, setIsHover] = useState(false);
+    const shouldDisplayClearButton = fileIds.length && isHover;
 
+    const handleClearFiles = useCallback(() => dispatch(clearFiles), []);
     const handleDeleteFile = useCallback(fileId => () => dispatch(deleteFiles([fileId])), []);
 
+    const handleMouseEnter = useCallback(() => setIsHover(true), []);
+    const handleMouseLeave = useCallback(() => setIsHover(false), []);
+
     return (
-        <div className={classes.fileList}>
+        <div className={classes.fileList} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className={classes.filesWrapper}>
                 {fileIds.map(fileId => (
                     <File file={filesById[fileId]} key={fileId} onDeleteFile={handleDeleteFile(fileId)} />
                 ))}
+                {fileIds.map(fileId => (
+                    <File file={filesById[fileId]} key={fileId} onDeleteFile={handleDeleteFile(fileId)} />
+                ))}
+                {fileIds.map(fileId => (
+                    <File file={filesById[fileId]} key={fileId} onDeleteFile={handleDeleteFile(fileId)} />
+                ))}
+                {fileIds.map(fileId => (
+                    <File file={filesById[fileId]} key={fileId} onDeleteFile={handleDeleteFile(fileId)} />
+                ))}
+                {fileIds.map(fileId => (
+                    <File file={filesById[fileId]} key={fileId} onDeleteFile={handleDeleteFile(fileId)} />
+                ))}
+                {fileIds.map(fileId => (
+                    <File file={filesById[fileId]} key={fileId} onDeleteFile={handleDeleteFile(fileId)} />
+                ))}
             </div>
+            {shouldDisplayClearButton && (
+                <IconButton className={classes.clearButton} label={t('clearFileList')} onClick={handleClearFiles}>
+                    <ClearIcon />
+                </IconButton>
+            )}
         </div>
     );
 };
