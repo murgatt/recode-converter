f8293fbf9638ea79928433e6f128c15fd76fa0fb
murgatt <theomurgat@hotmail.fr>
2020-04-06 21:51:20 +0200

    change when file can be deleted

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { Card, CardHeader, CircularProgress, LinearProgress, makeStyles } from '@material-ui/core';
@@ -8,6 +9,7 @@ import VideoIcon from '@material-ui/icons/MovieOutlined';
 import DeleteIcon from '@material-ui/icons/DeleteOutline';
 import IconButton from '../IconButton';
 import { FILE_STATUS } from '../../store/file/file.constants';
+import { getIsConversionRunning } from '../../store/conversion/conversion.selectors';
 
 const useStyles = makeStyles(theme => ({
     file: {
@@ -29,6 +31,8 @@ const File = ({ file, onDeleteFile }) => {
     const classes = useStyles();
     const { t } = useTranslation();
     const isConverting = status === FILE_STATUS.converting;
+    const isConversionRunning = useSelector(getIsConversionRunning);
+    const deleteActionIsDisabled = isConverting || isConversionRunning;
 
     const icon = useMemo(() => {
         switch (status) {
@@ -44,7 +48,7 @@ const File = ({ file, onDeleteFile }) => {
     }, [status]);
 
     const deleteAction = (
-        <IconButton disabled={isConverting} label={t('removeFile')} onClick={onDeleteFile}>
+        <IconButton disabled={deleteActionIsDisabled} label={t('removeFile')} onClick={onDeleteFile}>
             <DeleteIcon />
         </IconButton>
     );
--- src/store/conversion/conversion.selectors.js
+++ src/store/conversion/conversion.selectors.js
@@ -1,4 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 import { CONVERSION_STATUS } from './conversion.constants';
 
-export const getIsConversionPaused = ({ conversion }) => conversion.status === CONVERSION_STATUS.pause;
 export const getIsConversionRunning = ({ conversion }) => conversion.status === CONVERSION_STATUS.converting;
