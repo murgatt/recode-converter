aa2d30f34f547cfe188dc04f6354e7595b821b88
Théo Murgat <theomurgat@hotmail.fr>
2020-06-02 13:14:32 +0200

    feat(files): add file streams info in a collapse panel (#35)

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,23 +1,49 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { Card, CardHeader, CircularProgress, LinearProgress, makeStyles } from '@material-ui/core';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CircularProgress,
+    Collapse,
+    LinearProgress,
+    makeStyles,
+} from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/CheckCircle';
 import ErrorIcon from '@material-ui/icons/Error';
 import VideoIcon from '@material-ui/icons/MovieOutlined';
 import DeleteIcon from '@material-ui/icons/DeleteOutline';
+import ExpandIcon from '@material-ui/icons/ExpandMore';
 import IconButton from '../IconButton';
 import { FILE_STATUS } from '../../store/file/file.constants';
 import { getIsConversionRunning } from '../../store/conversion/conversion.selectors';
 import { formatFileSize } from '../../store/file/file.utils';
+import FileStreams from '../FileStreams';
 
 const useStyles = makeStyles(theme => ({
+    cardContent: {
+        marginBottom: theme.spacing(2),
+    },
+    expandButton: {
+        bottom: 4,
+        left: 0,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        position: 'absolute',
+        right: 0,
+        transform: props => (props.expanded ? 'rotate(180deg)' : 'rotate(0deg)'),
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
     file: {
         '&:last-child': {
             marginBottom: 0,
         },
         marginBottom: theme.spacing(),
+        position: 'relative',
     },
     progressWrapper: {
         height: 4,
@@ -29,7 +55,8 @@ const useStyles = makeStyles(theme => ({
 
 const File = ({ file, onDeleteFile }) => {
     const { name, progress, size, status } = file;
-    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+    const classes = useStyles({ expanded });
     const { t } = useTranslation();
     const isConverting = status === FILE_STATUS.converting;
     const isConversionRunning = useSelector(getIsConversionRunning);
@@ -49,6 +76,8 @@ const File = ({ file, onDeleteFile }) => {
         }
     }, [classes.successIcon, status]);
 
+    const handleExpandClick = useCallback(() => setExpanded(!expanded), [expanded]);
+
     const deleteAction = (
         <IconButton disabled={deleteActionIsDisabled} label={t('removeFile')} onClick={onDeleteFile}>
             <DeleteIcon />
@@ -58,6 +87,14 @@ const File = ({ file, onDeleteFile }) => {
     return (
         <Card className={classes.file} variant="outlined">
             <CardHeader action={deleteAction} avatar={icon} title={name} subheader={fileSize} />
+            <IconButton className={classes.expandButton} onClick={handleExpandClick} size="small">
+                <ExpandIcon />
+            </IconButton>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent className={classes.cardContent}>
+                    <FileStreams file={file} />
+                </CardContent>
+            </Collapse>
             <div className={classes.progressWrapper}>
                 {isConverting && <LinearProgress value={progress} variant="determinate" />}
             </div>
