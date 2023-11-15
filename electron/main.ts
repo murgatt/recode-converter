c9b59b84e4341ee6bed6e88db10d82bf672a54d2
Théo <12082276+murgatt@users.noreply.github.com>
2023-11-15 16:16:07 +0100

    chore(v2): setup libs (#100)
    * chore(v2): setup libs
    
    * fix EOF, prettier rules & tailwind config

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,8 @@
 import path from 'node:path';
 import { app, BrowserWindow } from 'electron';
 
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
 // The built directory structure
 //
 // ├─┬─┬ dist
@@ -13,12 +15,11 @@ import { app, BrowserWindow } from 'electron';
 process.env.DIST = path.join(__dirname, '../dist');
 process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
 
-let win: BrowserWindow | null;
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
 
 function createWindow() {
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -26,9 +27,9 @@ function createWindow() {
   });
 
   // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString());
-  });
+  // win.webContents.on('did-finish-load', () => {
+  //   win?.webContents.send('main-process-message', new Date().toLocaleString());
+  // });
 
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL);
@@ -36,21 +37,17 @@ function createWindow() {
     // win.loadFile('dist/index.html')
     win.loadFile(path.join(process.env.DIST, 'index.html'));
   }
+
+  win.maximize();
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-    win = null;
   }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
