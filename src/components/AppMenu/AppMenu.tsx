22f6fc6403a619180216c978b6e101e28750bb8d
Théo <12082276+murgatt@users.noreply.github.com>
2023-11-21 18:51:12 +0100

    feat(v2): file list (#103)
    * feat(v2): file list
    
    * add tests, drag animation

--- src/components/AppMenu/AppMenu.tsx
+++ src/components/AppMenu/AppMenu.tsx
@@ -1,17 +1,20 @@
 import { HomeIcon, SettingsIcon } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 import { NavigationMenu, NavigationMenuList } from '../ui/NavigationMenu';
 import { AppMenuItem } from './AppMenuItem';
 
 export const AppMenu = () => {
+  const { t } = useTranslation();
+
   return (
     <NavigationMenu className="flex flex-col justify-between border-r border-border px-2 py-3">
       <NavigationMenuList className="flex-col">
-        <AppMenuItem to="/">
+        <AppMenuItem label={t('appMenu.home')} to="/">
           <HomeIcon size="16" />
         </AppMenuItem>
       </NavigationMenuList>
       <NavigationMenuList className="flex-col">
-        <AppMenuItem to="/settings">
+        <AppMenuItem label={t('appMenu.settings')} to="/settings">
           <SettingsIcon size="16" />
         </AppMenuItem>
       </NavigationMenuList>
--- src/routes/Converter.tsx
+++ src/routes/Converter.tsx
@@ -1,8 +1,15 @@
+import { FileImport } from 'src/components/FileImport';
+import { FileList } from 'src/components/FileList';
+
 export const Converter = () => {
   return (
     <div className="flex h-full flex-col">
       <div className="flex grow overflow-hidden">
-        <section className="grow overflow-y-auto p-4">File List</section>
+        <section className="grow">
+          <FileImport>
+            <FileList />
+          </FileImport>
+        </section>
         <section className="w-80 shrink-0 overflow-y-auto border-l p-4">
           <h2 className="title-sm">Conversion Settings</h2>
         </section>
