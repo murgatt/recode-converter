1066d1d38875e738242028062df35fd8da0ea26b
murgatt <12082276+murgatt@users.noreply.github.com>
2023-11-30 08:30:14 +0100

    disable start conversion button when no file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import { PlayIcon } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
-import { getFiles, useStore } from 'src/store';
+import { getFilesToConvert, useStore } from 'src/store';
 import { Button } from '../ui/Button';
 import { DestinationInput } from './DestinationInput';
 
 export const Footer = () => {
   const { t } = useTranslation();
-  const files = useStore(getFiles);
+  const files = useStore(getFilesToConvert);
+  const isButtonDisabled = files.length === 0;
 
   const handleConversionStart = () => {
     window.conversion.startConversion({ files });
@@ -15,7 +16,7 @@ export const Footer = () => {
   return (
     <footer className="flex shrink-0 justify-between border-t p-4">
       <DestinationInput />
-      <Button className="flex gap-2" onClick={handleConversionStart} size="default">
+      <Button className="flex gap-2" disabled={isButtonDisabled} onClick={handleConversionStart} size="default">
         <PlayIcon size="16" />
         {t('footer.startConversion')}
       </Button>
