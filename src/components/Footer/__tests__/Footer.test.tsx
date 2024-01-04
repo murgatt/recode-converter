4d7476b9a19b14593588b7606326ab620647827f
murgatt <12082276+murgatt@users.noreply.github.com>
2024-01-04 09:47:53 +0100

    feat(v2): stop conversion

--- src/components/Footer/__tests__/Footer.test.tsx
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -1,30 +1,27 @@
 import { render, screen } from '@testing-library/react';
-import { fileStatusSchema } from 'schema';
-import { getFilesToConvert } from 'src/store';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
+import { describe, expect, it, vi } from 'vitest';
 import { Footer } from '../Footer';
-import type { VideoFile } from 'schema';
-
-vi.mock('src/store/selectors', () => ({
-  getFilesToConvert: vi.fn(),
-}));
 
 describe('Footer', () => {
-  beforeEach(() => {
-    vi.mocked(getFilesToConvert).mockReturnValue([]);
-  });
-
-  it('should display a disabled button by default', () => {
-    render(<Footer />);
+  it('should display a disabled start conversion button by default', () => {
+    render(<Footer isConversionRunning={false} isStartButtonDisabled onStopConversion={vi.fn()} />);
 
     expect(screen.getByRole('button', { name: 'Start conversion' })).toBeDisabled();
   });
 
-  it('should display a non disabled button if there is at least one file to convert', () => {
-    const file = { status: fileStatusSchema.enum.imported } as VideoFile;
-    vi.mocked(getFilesToConvert).mockReturnValue([file]);
-    render(<Footer />);
+  it('should display a non disabled start conversion button if there is at least one file to convert', () => {
+    render(<Footer isConversionRunning={false} isStartButtonDisabled={false} onStopConversion={vi.fn()} />);
 
     expect(screen.getByRole('button', { name: 'Start conversion' })).not.toBeDisabled();
+  });
+
+  it('should display a stop conversion button if conversion is running', async () => {
+    const handleStopConversion = vi.fn();
+    render(<Footer isConversionRunning={true} isStartButtonDisabled={false} onStopConversion={handleStopConversion} />);
+
+    await userEvent.click(screen.getByRole('button', { name: 'Stop conversion' }));
+
+    expect(handleStopConversion).toHaveBeenCalled();
   });
 });
--- src/components/__tests__/FileImport.test.tsx
+++ src/components/__tests__/FileImport.test.tsx
@@ -1,28 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import { getFiles } from 'src/store';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { FileImport } from '../FileImport';
-import type { VideoFile } from 'schema';
-
-vi.mock('src/store/selectors', () => ({
-  getFiles: vi.fn(),
-}));
 
 describe('FileImport', () => {
-  beforeEach(() => {
-    vi.mocked(getFiles).mockReturnValue([]);
-  });
-
   it('should display an import button by default', () => {
-    render(<FileImport>FileList</FileImport>);
+    render(<FileImport isFileListDisplayed={false}>FileList</FileImport>);
 
     expect(screen.getByText('Add files')).toBeVisible();
     expect(screen.queryByText('FileList')).not.toBeInTheDocument();
   });
 
   it('should display file list instead of import button if files have been added', () => {
-    vi.mocked(getFiles).mockReturnValue([{ name: 'FileName.mkv', path: '/path', size: 1024 } as VideoFile]);
-    render(<FileImport>FileList</FileImport>);
+    render(<FileImport isFileListDisplayed>FileList</FileImport>);
 
     expect(screen.getByText('FileList')).toBeVisible();
     expect(screen.queryByText('Add files')).not.toBeInTheDocument();
