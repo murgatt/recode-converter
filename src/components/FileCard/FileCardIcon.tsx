f8778452dfb3a19358b1d1f0b91cb9f38ea8f756
Théo <12082276+murgatt@users.noreply.github.com>
2024-01-21 22:44:31 +0100

    feat(v2): display conversion error (#118)

--- src/components/FileCard/FileCardIcon.tsx
+++ src/components/FileCard/FileCardIcon.tsx
@@ -1,12 +1,14 @@
 import { AlertOctagonIcon, CheckCircleIcon, FileVideoIcon, Loader2Icon } from 'lucide-react';
 import { fileStatusSchema } from 'schema';
+import { Tooltip } from '../ui/Tooltip';
 import type { FileStatus } from 'schema';
 
 type FileCardIconProps = {
+  error?: string;
   status: FileStatus;
 };
 
-export const FileCardIcon = ({ status }: FileCardIconProps) => {
+export const FileCardIcon = ({ error, status }: FileCardIconProps) => {
   switch (status) {
     case fileStatusSchema.enum.imported:
       return <FileVideoIcon className="shrink-0" data-testid="FileCardIcon_fileIcon" size="24" />;
@@ -15,6 +17,10 @@ export const FileCardIcon = ({ status }: FileCardIconProps) => {
     case fileStatusSchema.enum.conversionSuccess:
       return <CheckCircleIcon className="shrink-0 text-success" data-testid="FileCardIcon_checkIcon" size="24" />;
     case fileStatusSchema.enum.conversionError:
-      return <AlertOctagonIcon className="shrink-0 text-destructive" data-testid="FileCardIcon_alertIcon" size="24" />;
+      return (
+        <Tooltip content={error} delayDuration={0}>
+          <AlertOctagonIcon className="shrink-0 text-destructive" data-testid="FileCardIcon_alertIcon" size="24" />
+        </Tooltip>
+      );
   }
 };
