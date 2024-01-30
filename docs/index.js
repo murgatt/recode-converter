9b15d1dbc328ad43ffc84c123b923d43f85f1ebf
Théo <12082276+murgatt@users.noreply.github.com>
2024-01-30 11:29:53 +0100

    docs(v2): update README.md & misc repository info (#126)
    * docs(v2): update README.md & misc repository info
    
    * fix node version
    
    * remove blank title from issue templates
    
    * tiny png
    
    * old github pages site
    
    * add docs to .eslintignore
    
    * add docs to .prettierignore

---
+++ docs/index.js
@@ -0,0 +1,61 @@
+const GITHUB_API_URL = 'https://api.github.com/repos/murgatt/recode-converter/releases/latest';
+const LATEST_RELEASE_URL = 'https://github.com/murgatt/recode-converter/releases/latest';
+
+const PLATFORMS = {
+    linux: 'linux',
+    mac: 'mac',
+    windows: 'win',
+};
+
+const EXTENSIONS = {
+    mac: 'dmg',
+    win: 'exe',
+};
+
+const getPlatform = () => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (userAgent.match(/(mac|os x)/)) {
+        return PLATFORMS.mac;
+    }
+    if (userAgent.match(/windows/)) {
+        return PLATFORMS.windows;
+    }
+    if (userAgent.match(/linux/)) {
+        return PLATFORMS.linux;
+    }
+
+    return '';
+};
+
+const getAssetDownloadUrl = assets => {
+    const platform = getPlatform();
+    const extension = EXTENSIONS[platform];
+    const platformAsset = assets.find(asset => {
+        const splittedName = asset.name.split('.');
+
+        return splittedName[splittedName.length - 1] === extension;
+    });
+
+    if (!platformAsset) {
+        return LATEST_RELEASE_URL;
+    }
+
+    return platformAsset.browser_download_url;
+};
+
+const getDownloadUrl = async () => {
+    const response = await fetch(GITHUB_API_URL);
+    if (!response.ok) {
+        return LATEST_RELEASE_URL;
+    }
+    const data = await response.json();
+
+    return getAssetDownloadUrl(data.assets);
+};
+
+const setDownloadUrl = async () => {
+    const downloadUrl = await getDownloadUrl();
+    document.getElementById('download-button').setAttribute('href', downloadUrl);
+};
+
+setDownloadUrl();
