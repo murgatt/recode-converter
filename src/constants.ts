53e623b4d35716dbf068bf60a1e89305916d590d
Théo <12082276+murgatt@users.noreply.github.com>
2024-01-30 18:28:21 +0100

    feat(v2): add about page (#128)
    * feat(v2): add about page
    
    * fix app icon when app is built
    
    * fix typo in label

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const APP_WEBSITE_URL = 'https://murgatt.github.io/recode-converter/';
+export const AUTHOR_GITHUB_URL = 'https://github.com/murgatt/';
+export const GITHUB_REPOSITORY_URL = 'https://github.com/murgatt/recode-converter';
