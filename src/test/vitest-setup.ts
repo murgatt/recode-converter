4fa414f4510ef1efb04adb8710a0f52327365f1f
Théo <12082276+murgatt@users.noreply.github.com>
2024-02-06 23:11:06 +0100

    fix(v2): fix default destination path on Windows (#134)

--- src/test/vitest-setup.ts
+++ src/test/vitest-setup.ts
@@ -1,2 +1,5 @@
 import '@testing-library/jest-dom/vitest';
 import 'src/i18n';
+
+// @ts-ignore userAgentData is experimental, TypeScript does not have the type declarations
+navigator.userAgentData = { platform: 'macOS' };
